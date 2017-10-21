e6e708037022ecf4dd5ae507e4e6358a17895dea
han8909227 <hbao2016@hotmail.com>
2017-10-21 14:52:09 -0700

    first one works

--- MovingZerosToTheEnd/setup.py
+++ MovingZerosToTheEnd/setup.py
@@ -10,6 +10,6 @@ setup(
     author_email='hbao2016@hotmail.com',
     description='Given a list of random type objects, return a list with the int zeros at the end of list',
     install_requires=[],
-    extras_require={'test': ['pytest', 'pytest-watch', 'pytest-cov', 'tox', 'Faker']},
+    extras_require={'test': ['pytest', 'pytest-watch', 'pytest-cov', 'tox']},
     entry_points={'console_scripts': []}
 )
--- MovingZerosToTheEnd/src/test_zeros.py
+++ MovingZerosToTheEnd/src/test_zeros.py
@@ -1,19 +1,11 @@
 import pytest
 
-# Test.assert_equals(move_zeros(),[ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ])
-# Test.assert_equals(move_zeros([9,0.0,0,9,1,2,0,1,0,1,0.0,3,0,1,9,0,0,0,0,9]),[9,9,1,2,1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0])
-# Test.assert_equals(move_zeros(["a",0,0,"b","c","d",0,1,0,1,0,3,0,1,9,0,0,0,0,9]),["a","b","c","d",1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0])
-# Test.assert_equals(move_zeros(["a",0,0,"b",None,"c","d",0,1,False,0,1,0,3,[],0,1,9,0,0,{},0,0,9]),["a","b",None,"c","d",1,False,1,3,[],1,9,{},9,0,0,0,0,0,0,0,0,0,0])
-# Test.assert_equals(move_zeros([0,1,None,2,False,1,0]),[1,None,2,False,1,0,0])
-# Test.assert_equals(move_zeros(["a","b"]),["a","b"])
-# Test.assert_equals(move_zeros(["a"]),["a"])
-# Test.assert_equals(move_zeros([0,0]),[0,0])
-# Test.assert_equals(move_zeros([0]),[0])
-# Test.assert_equals(move_zeros([]),[])
 
-
-@pytest.mark.parametrize('array, result', [([1,2,3,4,5,0,9,9,9],[1,2,3,4,5,9,9,9,0])])
+@pytest.mark.parametrize('array, result', [([1, 2, 3, 4, 5, 0, 9, 9, 9], [1, 2, 3, 4, 5, 9, 9, 9, 0]),
+(["a",0,0,"b","c","d",0,1,0,1,0,3,0,1,9,0,0,0,0,9],["a","b","c","d",1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0]),
+([0,1,None,2,False,1,0],[1,None,2,False,1,0,0]), (["a","b"], ["a","b"]), (["a"],["a"]), ([0,0],[0,0]),
+([0],[0]), ([],[])])
 def test_move_zeros(array, result):
     """."""
-    from movezeros import move_zeros
+    from zeros import move_zeros
     assert move_zeros(array) == result
